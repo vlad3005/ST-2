2b631e429775423c3d5996160b68f45113b2459c
vlad3005 <vladborunov396@gmail.com>
2026-06-01 15:28:43 +0300

    fix 2

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vladb/UT2026/ST-2/build/_deps/googletest-src
-# Build directory: C:/Users/vladb/UT2026/ST-2/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vladb/UT2026/ST-2/build/_deps/googletest-src/googletest
-# Build directory: C:/Users/vladb/UT2026/ST-2/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
